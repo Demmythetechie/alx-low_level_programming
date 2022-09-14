f2078ac798eb633c02acadeedbc3a1aa6f23461c
Demmythetechie <demmythetechie@gmail.com>
2022-09-14 15:32:27 +0000

    Task 0 Done

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include<stdio>
+
+/**
+ * main - prints whether variable n is negative, positive or zero.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		print("%i is zero\n", n);
+	}
+	else
+	{
+		printf("%i is negative\n", n);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Task 0: Wrote a c program that prints whether variable n is positive, Negative or zero.
