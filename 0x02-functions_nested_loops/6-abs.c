d78fad0c36c2b275d5d0351ce7fc4c8a654d3371
Demmythetechie <demmythetechie@gmail.com>
2022-10-05 10:13:45 +0000

    task 6 done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include<stdlib.h>
+
+/**
+ * _abs - This program returns the absolute value of the the parameter x
+ * @x: The parameter that is converted to the absolute value.
+ *
+ * Return: (x)
+ */
+
+int _abs(int x)
+{
+	return (abs(x));
+}
