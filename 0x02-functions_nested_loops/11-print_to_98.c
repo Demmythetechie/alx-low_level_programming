6b037bae0501d231bc521fb2b854bf8c616592ca
Demmythetechie <demmythetechie@gmail.com>
2022-10-15 12:06:35 +0000

    task 11 Done

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,109 @@
+#include "main.h"
+#include<stdlib.h>
+
+/**
+ * print_to_98 - This program prints all natural numbers from n to 98.
+ * @n: parameter
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	if (n == 98)
+	{
+		int one_n = n / 10;
+		int two_n = n % 10;
+		_putchar(one_n + '0');
+		_putchar(two_n + '0');
+		_putchar('\n');
+	}
+	else if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			int nw = abs(n);
+			int one_n = nw / 10;
+			int two_n = nw % 10;
+
+			if (n < 0)
+			{
+				_putchar('-');
+			}
+
+			if (n < 10)
+			{
+				if (n < -9)
+				{
+					_putchar(one_n + '0');
+					_putchar(two_n + '0');
+				}
+				else
+				{
+					_putchar(two_n + '0');
+				}
+			}
+			else if (n < 100 && n >= 10)
+			{
+				_putchar(one_n + '0');
+				_putchar(two_n + '0');
+			}
+			else
+			{
+				int one_h = nw / 100;
+				int two_h = nw / 10;
+				int two_h2 = two_h % 10;
+				int three_h = nw % 10;
+				_putchar(one_h + '0');
+				_putchar(two_h2 + '0');
+				_putchar(three_h + '0');
+			}
+
+			if (n == 98)
+			{
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			int nw = abs(n);
+			int one_n = nw / 10;
+			int two_n = nw % 10;
+			if (n < 100)
+			{
+				_putchar(one_n + '0');
+				_putchar(two_n + '0');
+			}
+			else
+			{
+				int one_h = nw / 100;
+				int two_h = nw / 10;
+				int two_h2 = two_h % 10;
+				int three_h = nw % 10;
+				_putchar(one_h + '0');
+				_putchar(two_h2 + '0');
+				_putchar(three_h + '0');
+			}
+
+			if (n == 98)
+			{
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n--;
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -8,3 +8,4 @@ Task 6: This program converts parameter x to an absolute number
 Task 8: This program prints every minutes of the day from 00:00 to 23:59.
 Task 9: This program prints the 9 times table starting with 0.
 Task 10: This program returns the sum of two integer.
+Task 11: Prints all natural numbers from n (could be any number) to 98.
