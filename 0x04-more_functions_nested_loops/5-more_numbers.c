fe4e946b2ce804dacfb3ed8233cc02c39d305dd4
Demmythetechie <demmythetechie@gmail.com>
2022-10-21 20:30:21 +0000

    task 5 done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - This program prints number 1 - 14 x10
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int n_times = 0;
+	while (n_times <= 10)
+	{
+		int num = 0;
+		while (num <= 14)
+		{
+			int num1 = num / 10;
+			int num2 = num % 10;
+
+			if (num > 9 && num <= 14)
+			{
+				_putchar(num1 + '0');
+			}
+			_putchar(num2 + '0');
+			num++;
+		}
+		n_times++;
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -2,4 +2,5 @@ Task 0: This program checks if the parameter passed into the function is upperca
 Task 1: This program returns 1 if the argument is a digit else 0.
 Task 2: This program returns the product of integer argument a and b.
 Task 3:
-Task 4: This program prints number  1 - 9 skipping 2 and 4. 
+Task 4: This program prints number  1 - 9 skipping 2 and 4.
+Task 5: This program prints number 1 - 14 x10.
