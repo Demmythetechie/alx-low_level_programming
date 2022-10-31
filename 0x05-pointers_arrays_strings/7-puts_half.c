21fe68b29b3596c17ef6eb2c1475a72707c4f182
Demmythetechie <demmythetechie@gmail.com>
2022-10-31 12:40:54 +0000

    task 7 done

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include"main.h"
+#include<string.h>
+
+/**
+ * puts_half - This function prints the second half of the string str.
+ * @str: The string being printed
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int len = strlen(str);
+	int n_times = 0;
+	int result = len % 2;
+	int half = len / 2;
+
+	if (result == 0)
+	{
+		while (n_times < len)
+		{
+			if (n_times < half)
+			{
+			}
+			else
+			{
+				_putchar(*(str + n_times));
+			}
+			n_times++;
+		}
+	}
+	else
+	{
+		_putchar(*(str + len));
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -5,3 +5,4 @@ task 3: This program prints the string passed into the function _ puts().
 task 4: This program prints the string passed into function printrev() in reverse.
 task 5: This program returns the string passed into function revstring() in reverse.
 task 6: This program prints one character of string str and skip one till the end of the string.
+task 7: This program prints the second half of the string passed into the function.
