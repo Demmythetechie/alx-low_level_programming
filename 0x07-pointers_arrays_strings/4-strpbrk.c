69d6429819ffc4ef70fb5da813d9d7b5b42e1779
Demmythetechie <demmythetechie@gmail.com>
2022-11-08 19:10:37 +0000

    task 4 done

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,17 @@
+#include<string.h>
+
+/**
+ * _strpbrk - This program returns a string when a char from accept matches s
+ * @s: The string that scans the string accept for matching character
+ * @accept: the string being scanned
+ *
+ * Return: remain
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *remain;
+	char *result = strpbrk(s, accept);
+
+	return (strcpy(remain, result));
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@ task 0: This program fills memory with a constant byte
 task 1: This program copy and replace memory with src array memory
 task 2: This program scans the string s for char c using strchr
 task 3: This program returns number of matching character using s & accept
+task 4: This program returns a string when a character from accept matches s
