18a8fe21024623a1855c1eb013b6aee48fe6389a
Demmythetechie <demmythetechie@gmail.com>
2022-11-08 22:18:51 +0000

    task 6 done

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include"main.h"
+#include<string.h>
+
+/**
+ * print_chessboard - Print the 2d array in the main() using putchar
+ * @a: The array to be printed
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int len = 8;
+	int c = 0;
+
+	while (c < len)
+	{
+		int r = 0;
+		while (r < len)
+		{
+			_putchar((*(a + c))[r]);
+			r++;
+		}
+		_putchar('\n');
+		c++;
+	}
+}
