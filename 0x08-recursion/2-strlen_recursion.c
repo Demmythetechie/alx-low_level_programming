39f182b2a4e7150102caf90578597c316c88a777
Demmythetechie <demmythetechie@gmail.com>
2022-11-11 22:11:03 +0000

    task 2 done

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+
+/**
+ * _strlen_rescursion - This program returns the length of string s.
+ * @s: the length of the string being returned.
+ *
+ * Return: Always 0
+ */
+
+int _strlen _recursion(char *s)
+{
+	return (strlen(s));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,2 +1,3 @@
 task 0: This program prints a string using puts followed by a new line.
-task 1: This program prints the string s in reverse.
+task 1: This program prints the string s in reverse
+task 2: This program return the length of string s.
