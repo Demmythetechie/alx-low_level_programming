ff88f2642aff8a5e380a70844a23db73865c50a5
Demmythetechie <demmythetechie@gmail.com>
2022-11-11 22:15:47 +0000

    task 2 done

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 #include<string.h>
 
 /**
- * _strlen_rescursion - This program returns the length of string s.
+ * _strlen_recursion - This program returns the length of string s.
  * @s: the length of the string being returned.
  *
  * Return: Always 0
