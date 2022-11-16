fbd68faa6cb9b45d2cfba2b3dc5bd38f1a811c9d
Demmythetechie <demmythetechie@gmail.com>
2022-11-16 22:06:03 +0000

    task 3 done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+/**
+ * main - This program prints the product of two command line argument
+ * @argc: This parameter tells the number of command line argument.
+ * @argv: This parameter store the name of command line arguments in it.
+ *
+ * Return: 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int product = 1;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		while (i < argc)
+		{
+			product = product * atoi(argv[i]);
+			i++;
+		}
+		printf("%i\n", product);
+		return (0);
+	}
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,3 +1,4 @@
 task 0: This program prints the name of the executable file.
 task 1: This program prints the number of argument passed when launching the exec file.
 task 2: This program prints all the command line argument stored in argv string array.
+task 3: This program prints the product of two command line argument.
