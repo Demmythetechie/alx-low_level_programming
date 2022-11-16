7e9bb963c661945f536ca0104391ed87f3ba0d41
Demmythetechie <demmythetechie@gmail.com>
2022-11-16 21:11:26 +0000

    task 1 done

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -11,25 +11,6 @@
 
 int main(int argc, __attribute__((unused))char *argv[])
 {
-	int unit = argc / 10;
-	int tenths = argc % 10;
-	int hundred = argc / 100 % 10;
-
-	if (argc > 9 && argc < 100)
-	{
-		_putchar(unit + '0');
-		_putchar(tenths + '0');
-	}
-	else if (argc > 99 && argc < 1000)
-	{
-		_putchar(unit + '0');
-		_putchar(tenths + '0');
-		_putchar(hundred + '0');
-	}
-	else
-	{
-		_putchar(argc + '0');
-	}
-	_putchar('\n');
+	printf("%i\n", argc);
 	return (0);
 }
