0126b0ab09ce4016ca70d0815e5b52e83a4aa09b
Demmythetechie <demmythetechie@gmail.com>
2022-11-22 23:35:32 +0000

    task 1 done

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+/**
+ * _strdup - Returns a pointer to a allocated memory, containing a cpy of str.
+ * @str: The string that is being duplicated.
+ *
+ * Return: result
+ */
+
+char *_strdup(char *str)
+{
+	char *result;
+
+	result = strdup(str);
+
+	if (str == NULL)
+	{
+		return (result);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,1 +1,2 @@
 task 0: This program creates a string and initializes it using c.
+task 1: This program returns a pointer to a allocated memory, containing a cpy of the str.
