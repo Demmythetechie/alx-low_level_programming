ae53dd5e6ecc0fe83d4530e7eb41b126a7371d04
Demmythetechie <demmythetechie@gmail.com>
2022-11-23 11:31:50 +0000

    task 2 done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,7 +7,7 @@
  * @s1: destination for concatenation
  * @s2: src of concatenation
  *
- * Return: return NULL or concat
+ * Return: return NULL or concat int len2 = strlen(s2);
  */
 
 char *str_concat(char *s1, char *s2)
@@ -18,10 +18,38 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
+	else if (s1 != NULL && s2 != NULL)
+	{
+		int i = 0;
+		int n = 0;
+		int len = strlen(s1) + strlen(s2);
+		int len1 = strlen(s1);
+
+		concat = malloc(sizeof(char) * len);
+
+		while (i < len1)
+		{
+			*(concat + i) = *(s1 + i);
+			i++;
+		}
+		while (i < len)
+		{
+			*(concat + i) = *(s2 + n);
+			i++;
+			n++;
+		}
+		return (concat);
+	}
 	else
 	{
-		strcat(s1, s2);
-		concat = strdup(s1);
+		if (s1 == NULL)
+		{
+			concat = strdup(s2);
+		}
+		else if (s2 == NULL)
+		{
+			concat = strdup(s1);
+		}
 		return (concat);
 	}
 }
