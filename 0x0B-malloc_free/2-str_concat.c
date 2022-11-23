28d1d992ca3b9953c1e69b2d6f3af9f24e174489
Demmythetechie <demmythetechie@gmail.com>
2022-11-23 11:48:28 +0000

    task 2 done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,7 +16,7 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL && s2 == NULL)
 	{
-		return (concat = strdup("\n"));
+		return (concat = strdup(""));
 	}
 	else if (s1 != NULL && s2 != NULL)
 	{
