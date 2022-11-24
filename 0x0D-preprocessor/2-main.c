35d12fade03239e2541cd32db5bd9d5fb3e9d75d
Demmythetechie <demmythetechie@gmail.com>
2022-11-24 10:51:26 +0000

    task 2 done

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - This program prints the name of this file with new line
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
