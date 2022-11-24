08303e373c9f5f77131c8b07cf441f941129a981
Demmythetechie <demmythetechie@gmail.com>
2022-11-24 14:21:40 +0000

    task 4 done

---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include"4-sum.h"
+#include"4-sum.h"
+
+/**
+ * main - This program compute the sum of x and y in the macro function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int s;
+
+	s = SUM(98, 1024);
+	printf("%d\n", s);
+	return (0);
+}
