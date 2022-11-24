88fbda4a2ae2bfefbc9a6000d8d9546b342e5f0d
Demmythetechie <demmythetechie@gmail.com>
2022-11-24 13:19:56 +0000

    task 3 done

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
 #ifndef _3_FUNCTION_LIKE_MACRO_H_
 #define _3_FUNCTION_LIKE_MACRO_H_
 
-#include<stdlib.h>
-#define ABS(x) abs(x)
+#include<math.h>
+#define ABS(x) fabs(((double)(x)))
 
 #endif /* _3_FUNCTION_LIKE_MACRO_H_ */
