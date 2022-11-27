53e377c855951360f60364eaaeb58491bae1e67f
Demmythetechie <demmythetechie@gmail.com>
2022-11-27 13:49:36 +0000

    task 1 done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			*(string + i) = *(s1 + i);
 			i++;
 		}
-		while (m <= nm)
+		while (m < nm)
 		{
 			*(string + i) = *(s2 + m);
 			m++;
