631f2242af9b50d4f9eaaff258913bdd820b2b9f
Demmythetechie <demmythetechie@gmail.com>
2022-11-28 08:00:41 +0000

    task 3 done

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,37 @@
+#include<stdlib.h>
+
+/**
+ * array_range - This function creates an array of integer using min and max
+ * @min: The starting element of the array
+ * @max: The ending number of an array
+ *
+ * Return: arr
+ */
+
+int *array_range(int min, int max)
+{
+	if (max > min)
+	{
+		int n = 0;
+		int i = min;
+		int *arr;
+
+		arr = (int *)calloc(max - min, sizeof(int));
+
+		while (n <= max - min)
+		{
+			*(arr + n) = i;
+			n++;
+			i++;
+		}
+		return (arr);
+	}
+	else if (min > max)
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,3 +1,4 @@
 task 0: This program returns a * to the allocated memory.
 task 1:
 task 2: This function allocates memmory using an array of element and size.
+task 3: This function creates an array of integer using min and max variables. 
