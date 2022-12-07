83dc6d7cf916b7f7b6ec4f4cb3228eb6079e5db7
Demmythetechie <demmythetechie@gmail.com>
2022-12-07 09:20:57 +0000

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -119,8 +119,8 @@ char *test4(__attribute__((unused)) char *s1, char *s2, unsigned int n)
 	}
 	else
 	{
-		str = malloc((n + 2) * sizeof(char));
-		while (num <= n)
+		str = malloc((n + 1) * sizeof(char));
+		while (num < n)
 		{
 			*(str + num) = s2[num];
 			num++;
