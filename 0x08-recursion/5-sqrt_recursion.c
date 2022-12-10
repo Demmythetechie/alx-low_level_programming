16370bafbcf63a7f4cfba2d03bc2d31406310b53
Demmythetechie <demmythetechie@gmail.com>
2022-12-10 13:34:37 +0000

    5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,15 +1,46 @@
 /**
- * is_prime_number - This program returns 1 if n is a prime number else 0
- * @n: The integer being checked
+ * _sqrt - This function finds the sqrt of the argument n
+ * @n: The squre of n
  *
- * Return: 0 or 1.
+ * Return: sq
  */
 
-int is_prime_number(int n)
+int _sqrt(int n)
 {
-	if (n)
+	int i = 0;
+	int sq = -1;
+
+	while (i <= n)
 	{
-		return (0);
+		if ((i * i) == n)
+		{
+			sq = i;
+			break;
+		}
+		else
+		{
+			sq = -1;
+		}
+		i++;
 	}
-	is_prime_number(n - 1);
+	return (sq);
+}
+
+/**
+ * _sqrt_recursion - This find returns the square root of n
+ * @n: n
+ *
+ * Return: sqr
+ */
+
+int _sqrt_recursion(int n)
+{
+	int m = n;
+
+	if (n == m)
+	{
+		return (_sqrt(n));
+	}
+	return (_sqrt_recursion(n));
+}
 
