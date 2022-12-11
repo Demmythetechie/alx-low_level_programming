fe7229b142f22f58ea554f62af8b8ee588cb94d6
demmythetechie@gmail.com <demmythetechie@gmail.com>
2022-12-11 13:52:41 +0000

    1. A dog is the only thing on earth ...

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include<stddef.h>
+#include<stdio.h>
 
 /**
  * init_dog - This functions assigns variable to a struct variable
@@ -12,7 +14,14 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		(*d).name = name;
+		 d->age = age;
+		 d->owner = owner;
+	}
+	else
+	{
+		printf("Ok\n");
+	}
 }
