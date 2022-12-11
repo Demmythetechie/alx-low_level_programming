0f3f2b68725ab9b9541dd2decfdd525ef37f5659
demmythetechie@gmail.com <demmythetechie@gmail.com>
2022-12-11 15:13:34 +0000

    2. A dog will teach you unconditional love...wq

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - This function prints out the variable in struct dog
+ * @d: The structure being printed
+ *
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if ((*d).name != NULL && d->age != 0 && (*d).owner != NULL)
+	{
+		printf("Name: %s\n", (*d).name);
+		printf("Age: %f\n", (*d).age);
+		printf("Owner: %s\n", (*d).owner);
+	}
+	else if (d == NULL)
+	{
+	}
+	else if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+	else if (d->age == 0)
+	{
+		printf("Age: (nil)\n");
+	}
+	else if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else if (d->name == NULL)
+	{
+		printf("Name: %s\n", (*d).name);
+	}
+	else if (d->age == 0)
+	{
+		printf("Age: %f\n", (*d).age);
+	}
+	else if (d->owner == NULL)
+	{
+		printf("Owner: %s\n", (*d).owner);
+	}
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,1 +1,2 @@
 task 0: This task was to build a template for dog to get their age, name, owner.
+task 1: This task was all about dereferencing a struct into a function using a pointer
