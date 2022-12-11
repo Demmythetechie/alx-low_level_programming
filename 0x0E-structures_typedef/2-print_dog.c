c2c97771120e8c9759e6086266857b172b1cd6d9
demmythetechie@gmail.com <demmythetechie@gmail.com>
2022-12-11 18:17:49 +0000

    2. A dog will teach you unconditional love...wq

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,28 +19,22 @@ void print_dog(struct dog *d)
 	else if (d == NULL)
 	{
 	}
-	else if (d->name == NULL)
+	else if (d->name == NULL && d->age == 0)
 	{
 		printf("Name: (nil)\n");
-	}
-	else if (d->age == 0)
-	{
 		printf("Age: %d\n", 0);
+		printf("Owner: %s\n", (*d).owner);
 	}
-	else if (d->owner == NULL)
+	else if (d->name == NULL && d->owner == NULL)
 	{
+		printf("Name: (nil)\n");
+		printf("Age: %f\n", (*d).age);
 		printf("Owner: (nil)\n");
 	}
-	else if (d->name != NULL)
+	else if (d->age == 0 && d->owner == NULL)
 	{
-		printf("Name: %s\n", (*d).name);
-	}
-	else if (d->age != 0)
-	{
-		printf("Age: %f\n", (*d).age);
-	}
-	else if (d->owner != NULL)
-	{
-		printf("Owner: %s\n", (*d).owner);
+		printf("Name: %s\n", (*d).name)
+		printf("Age: %d\n", 0);
+		printf("Owner: (nil)\n");
 	}
 }
