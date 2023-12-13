6b787113a70c2886ca05e33c7719d32e584a2eff
Demmythetechie <demmythetechie@gmail.com>
2023-12-13 10:15:25 +0100

    Updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,23 @@
 #include "lists.h"
-#include <stdio.h>
-
 /**
- * print_list - function that prints all the element
- * @h: pointer
- * Return: nodes
-*/
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t nelem;
 
-	while (h)
+	nelem = 0;
+	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		nodes++;
 		h = h->next;
+		nelem++;
 	}
-	return (nodes);
+	return (nelem);
 }
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,17 +1,19 @@
 #include "lists.h"
 /**
- * list_len - function that returns the number of elements
- * @h: the link list
- * Return: num of elements
-*/
+ * list_len - returns then number of elements in a list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+
 size_t list_len(const list_t *h)
 {
-	size_t i = 0;
+	size_t nelem;
 
-	while (h)
+	nelem = 0;
+	while (h != NULL)
 	{
-		i++;
 		h = h->next;
+		nelem++;
 	}
-	return (i);
+	return (nelem);
 }
